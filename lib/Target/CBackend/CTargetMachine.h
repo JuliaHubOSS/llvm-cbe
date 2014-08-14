e358004c0fd73d1f17cde7af9aa07da9601a0739
Aimee DiPietro <aimee.dipietro@gmail.com>
2014-08-14 10:29:27 -0400

    initial llvm-cbe commit

---
+++ lib/Target/CBackend/CTargetMachine.h
@@ -0,0 +1,45 @@
+//===-- CTargetMachine.h - TargetMachine for the C backend ------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the TargetMachine that is used by the C backend.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CTARGETMACHINE_H
+#define CTARGETMACHINE_H
+
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/IR/DataLayout.h"
+
+namespace llvm {
+
+struct CTargetMachine : public TargetMachine {
+  CTargetMachine(const Target &T, StringRef TT,
+                 StringRef CPU, StringRef FS, const TargetOptions &Options,
+                 Reloc::Model RM, CodeModel::Model CM,
+                 CodeGenOpt::Level OL)
+    : TargetMachine(T, TT, CPU, FS, Options) { }
+
+  virtual bool addPassesToEmitFile(PassManagerBase &PM,
+                                   formatted_raw_ostream &Out,
+                                   CodeGenFileType FileType,
+                                   bool DisableVerify,
+								   AnalysisID StartAfter,
+								   AnalysisID StartBefore
+								   );
+
+  virtual const DataLayout *getDataLayout() const { return 0; }
+};
+
+extern Target TheCBackendTarget;
+
+} // End llvm namespace
+
+
+#endif
