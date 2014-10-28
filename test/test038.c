19502162cddf49bed2b078b0fed703f69a2cb369
twalters25 <twalters@draper.com>
2014-10-28 13:41:27 -0400

    This code tests to see that the CBE can handle the Compound Modulus Assignment(a%=b) operator.

---
+++ test/test038.c
@@ -0,0 +1,26 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C ----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE can handle the
+// Compound Modulus Assignment(a%=b) operator.
+// *TW
+//===------------------------------------------------------------------------===//
+
+int main(){
+
+    int a = 20;
+    int b = 14;
+
+    a%=b;
+    if(a == 6){
+        return 6;
+    }
+    return 1;
+}
+
