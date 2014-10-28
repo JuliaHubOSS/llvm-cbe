dbd78300b25cc9a14d3cb043f91c3b92b507c5be
twalters25 <twalters@draper.com>
2014-10-28 13:41:27 -0400

    This code tests to see that the CBE can handle the NOT (~a) bitwise operator.

---
+++ test/test028.c
@@ -0,0 +1,26 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C ----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE can handle the
+// NOT (~a) bitwise operator.
+// *TW
+//===------------------------------------------------------------------------===//
+
+int main(){
+
+    unsigned int a = -7;
+    unsigned int b = 0;
+
+    b = ~a;
+    if( b == 6){
+        return 6;
+    }
+    return 1;
+}
+
