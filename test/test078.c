2e7a0f6ec5e5723cd3c6e4e093ca331a5aadff75
twalters25 <twalters@draper.com>
2014-10-28 13:41:28 -0400

    This code tests to see that the CBE will properly use a NULL pointer and confirm that it is equal to zero.

---
+++ test/test078.c
@@ -0,0 +1,23 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE will properly use a NULL pointer and
+// confirm that it is equal to zero.
+// *TW
+//
+//===----------------------------------------------------------------------===//
+#include <stddef.h>
+
+int main() {
+   int *ptr = NULL;
+    if (ptr == 0){
+        return 6;
+    }
+    return 1;
+}
