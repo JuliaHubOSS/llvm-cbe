eca0706860acc24189756a22e50876dc7409f502
twalters25 <twalters@draper.com>
2014-10-28 13:41:27 -0400

    This code tests to see that the CBE can handle the AND (a&b) bitwise operator.

---
+++ test/test029.c
@@ -0,0 +1,27 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C ----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE can handle the
+// AND (a&b) bitwise operator.
+// *TW
+//===------------------------------------------------------------------------===//
+
+int main(){
+
+    unsigned int a = 6;  //0110
+    unsigned int b = 15; //1111
+    unsigned int c = 0;
+
+    c = a&b;
+    if(c == 6){
+        return 6;
+    }
+    return 1;
+}
+
