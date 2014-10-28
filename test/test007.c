c5d5fd7a8e541db6adeec98ae96bc4fddcd2c836
twalters25 <twalters@draper.com>
2014-10-28 13:41:27 -0400

    This code tests to see that the CBE will execute a switch statement correctly.

--- test/test007.c
+++ test/test007.c
@@ -8,30 +8,26 @@
 //===----------------------------------------------------------------------===//
 //
 // This code tests to see that the CBE will execute a switch statement correctly.
-//
+// *TW
 //===----------------------------------------------------------------------===//
 
-#include <stdio.h>
-
-int main()
-{
+int main() {
    char var = 'x';
    
-   switch (var)
-   {
-   case 'z' :
-    return 0;
-    break;
-   case 'y' :
-    return 1;
-    break;
-   case 'x' :
-    return 6;
-    break;
-   case 'w' :
-    return 7;
-    break;
-   default :
-    return 100;
+   switch (var) {
+      case 'z' :
+         return 0;
+         break;
+      case 'y' :
+         return 1;
+         break;
+      case 'x' :
+         return 6;
+         break;
+      case 'w' :
+         return 7;
+         break;
+      default :
+         return 100;
    }
 }
