d5c15aed59d7ca6fc3bff86c46c31b4d39ce1b6f
twalters25 <twalters@draper.com>
2014-10-28 13:41:28 -0400

    This code tests to see that the CBE will execute an if statement correctly.

---
+++ test/test071.c
@@ -0,0 +1,19 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE will execute an if statement correctly.
+// *TW 
+//
+//===----------------------------------------------------------------------===//
+
+int main() {
+   int x = 6;
+   if (x == 6)
+      return x;
+}
