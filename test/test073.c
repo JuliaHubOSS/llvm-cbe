c2d85f35c7422b87267a8f02d4dc1cb889f07a33
twalters25 <twalters@draper.com>
2014-10-28 13:41:28 -0400

    This code tests to see that the CBE will execute a do-while statement correctly.

---
+++ test/test073.c
@@ -0,0 +1,20 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C --------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===---------------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE will execute a do-while statement correctly.
+// *TW
+//
+//===---------------------------------------------------------------------------===//
+
+int main() {
+   int x = 0;
+   do {
+      return 6;
+   } while (x == 0);
+}
