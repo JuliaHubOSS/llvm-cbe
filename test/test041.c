bd4b8bfdc9ae0d14dcc68fd41b4f062ed78c520a
twalters25 <twalters@draper.com>
2014-10-28 13:41:27 -0400

    This code tests to see that the CBE can handle the XOR (a^=b) Compound Assignment bitwise operator.

---
+++ test/test041.c
@@ -0,0 +1,26 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C ----------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE can handle the
+// XOR (a^=b) Compound Assignment bitwise operator.
+// *TW
+//===------------------------------------------------------------------------===//
+
+int main(){
+
+    unsigned int a = 9;  //1001
+    unsigned int b = 15; //1111
+
+    a^=b;
+    if(a == 6){
+        return 6;
+    }
+    return 1;
+}
+
