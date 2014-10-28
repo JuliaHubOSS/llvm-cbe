4a6e3ad1500724c21b46cc6c13c1dbd33a16e3f2
twalters25 <twalters@draper.com>
2014-10-28 13:41:28 -0400

    This code tests to see that the CBE will properly use a double pointer and confirms the value located originally at the address of x.

---
+++ test/test079.c
@@ -0,0 +1,25 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE will properly use a double pointer and
+// confirms the value located originally at the address of x.
+// *TW
+//
+//===----------------------------------------------------------------------===//
+
+int main() {
+   double x = 6, y = 0, *dp;
+   dp = &x;
+   y = *dp;
+   if (y == 6){
+      return 6;
+   }
+   return 1;
+}
+
