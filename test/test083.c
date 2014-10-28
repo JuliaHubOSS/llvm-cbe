7cd9917df270a15d1e936f05cade8bb38fcedc4f
twalters25 <twalters@draper.com>
2014-10-28 13:41:28 -0400

    This code tests to see that the CBE will properly use a pointer to access an array.

---
+++ test/test083.c
@@ -0,0 +1,24 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE will properly use a pointer to
+// access an array.
+// *TW
+//
+//===----------------------------------------------------------------------===//
+
+int main(){
+   int *ip;
+   int a[2];
+   a[0] = 1;
+   a[1] = 6;
+   ip = &a[1];
+
+   return *ip;
+}
