6724fd4442411e0ea32beb88b5dc63af6437cff0
twalters25 <twalters@draper.com>
2014-10-31 15:34:40 -0400

    Tests if the CBE will execute Macro Substitution.

---
+++ test/test103.c
@@ -0,0 +1,23 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C -------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===---------------------------------------------------------------------===//
+//
+// Tests if the CBE will execute Macro Substitution.
+// *TW
+//
+//===---------------------------------------------------------------------===//
+#define A 3
+#define B 3
+#define C A + B
+
+int main(){
+
+   int x = C;
+   return x;
+}
+
