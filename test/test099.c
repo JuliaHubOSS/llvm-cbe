7d5c3850bd5c5bca87556ffe6ee4273a6aa4931e
twalters25 <twalters@draper.com>
2014-10-31 11:00:49 -0400

    Tests if the CBE can handle register variables.

---
+++ test/test099.c
@@ -0,0 +1,21 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C ----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------------===//
+//
+// Test if CBE can handle register variables.
+// *TW
+//===------------------------------------------------------------------------------===//
+
+int main() {
+    int i;
+    register int counter = 0;
+    counter += 6;
+
+    return 6;
+}
+
