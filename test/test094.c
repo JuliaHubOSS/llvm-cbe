5725408ed1de5f5fa72d860ac5da69ac1db989a7
twalters25 <twalters@draper.com>
2014-10-31 11:00:49 -0400

    Tests if the CBE will execute a structure using typedef.

---
+++ test/test094.c
@@ -0,0 +1,29 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C ----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE will execute a structure using typedef.
+// *TW
+//===------------------------------------------------------------------------------===//
+
+typedef struct test {
+ int var1;
+ int var2;
+ int var3;
+}testrename;
+
+int main(){
+    testrename variable;
+
+    variable.var2 = 5;
+    variable.var3 = 6;
+    variable.var1 = 9;
+
+    return variable.var3;
+}
+
