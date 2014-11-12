3f562948696943365f882d68ea3f447f91e6c7a7
Timothy Walters <twalters25@users.noreply.github.com>
2014-11-12 14:07:43 -0500

    This code tests if the CBE will execute a tail recursion example correctly.
    This test currently fails due to an error involving if/else loops. The return statement and value are located at the bottom of the 'tail' function when converted to test104.cbe.c, independent of the if or else scopes, causing a loss of necessary flow control.

---
+++ test/test104.c
@@ -0,0 +1,24 @@
+//===-- CBackend.cpp - Library for converting LLVM code to C --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This code tests if the CBE will execute a tail recursion example correctly.
+// *TW
+//
+//===----------------------------------------------------------------------===//
+
+int tail (int n) {
+  if (n == 6)
+    return n;
+  else
+    return tail(n+1);
+}
+
+int main(){
+  return tail(0);
+}
