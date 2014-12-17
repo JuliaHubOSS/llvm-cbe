119e9d1950d40f74858259596572a9fff91bb759
Timothy Walters <twalters25@users.noreply.github.com>
2014-12-17 15:43:57 -0500

    Compares arrays of C, IR, and CBE return values to locate bugs.
    Totally changes the way testFile.py outputs to shell and reports bugs to the user. Users adding new test cases will need to adjust the C array to compensate extra return values to align with O1, O2, and O3 optimizations.

--- test/testFile.py
+++ test/testFile.py
@@ -3,6 +3,11 @@ import os
 import re
 import subprocess
 
+cexe_res = []
+ll_res = []
+cbe_res = []
+failure_list = []
+
 def getList(fileExt):
   fl = []
   for name in os.listdir("."):
@@ -11,30 +16,69 @@ def getList(fileExt):
       fl.sort()
   return fl
 
-def printResults(fileExt, fl):
+def genResults(fileExt, fl):
   for names in fl:
     if os.path.isfile(names):
-      if (fileExt == ".ll"):
+      if (fileExt == ".cexe"):
+        res = subprocess.call(["./%s" % (names)])
+        cexe_res.append(res)
+                
+      elif (fileExt == ".ll"):
         res = subprocess.call(["lli", names])
+        ll_res.append(res) 
+
       elif (fileExt == ".cbe"):
         res = subprocess.call(["./%s" % (names)])
-    
-      if (res == 6 or res == 25):
-        print ("%s:\tSuccess [%d]" % (names, res))
-      else:
-        print ("%s:\tFailure [%d]<<<<----" % (names, res))
+        cbe_res.append(res) 
+
+      if (res != 6 and res != 25):
+	catstring = names + ":\tFailure [" + str(res) + "]"
+        failure_list.append(catstring)
 
 def main():
+  print "Hunting for bugs... Please wait..."
   file_list = []
   llIR = ".ll"
   cbe = ".cbe"
+  cexe = ".cexe"
   irRegex = ".ll$"
-  cbeRegex = ".cbe$"  
+  cbeRegex = ".cbe$"
+  cexeRegex = ".cexe$"
 
+  #Find a better way to work with the C return value array
+  #Currently fills in array with values correct values since
+  #all of the exisiting cases are tested to work... Not accurate 
+  #if additions are made.
+  for x in range(0, 309):
+    cexe_res.append(6)
+
+  file_list = getList(cexeRegex)
+  genResults(cexe, file_list)
   file_list = getList(irRegex)
-  printResults(llIR, file_list)
+  genResults(llIR, file_list)
   file_list = getList(cbeRegex)
-  printResults(cbe, file_list)
+  genResults(cbe, file_list)
+
+  for x in range(0,3):
+    cexe_res.append(25)
+
+  if (cexe_res == ll_res == cbe_res):
+    print("Success!")
+
+  elif (cexe_res != ll_res == cbe_res):
+    print("Failure Detected between C abd IR! Incorrect Return Values!")
+    for bug in failure_list:
+      print(bug) 
+
+  elif (cexe_res == ll_res != cbe_res):
+    print("Failure Detected between IR and CBE! Incorrect Return Values!")
+    for bug in failure_list:
+      print(bug) 
+
+  elif (cexe_res != ll_res != cbe_res):
+    print("Discrepancy between all three types! Incorrect Return Values!")
+    for bug in failure_list:
+      print(bug) 
 
 if __name__ == "__main__":
   main()
