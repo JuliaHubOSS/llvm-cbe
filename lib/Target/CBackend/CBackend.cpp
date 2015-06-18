9f1ebce0aa0170d50261a40dbb29f420170bd43a
Jameson Nash <vtjnash@gmail.com>
2015-06-18 13:38:12 -0400

    split CBackend file into code and internal header

--- lib/Target/CBackend/CBackend.cpp
+++ lib/Target/CBackend/CBackend.cpp
@@ -12,44 +12,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "CTargetMachine.h"
-#include "llvm/IR/Attributes.h"
-#include "llvm/IR/CallingConv.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/TypeFinder.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/Pass.h"
-#include "llvm/PassManager.h"
-#include "llvm/IR/InstVisitor.h"
-#include "llvm/IR/Intrinsics.h"
-#include "llvm/IR/IntrinsicInst.h"
-#include "llvm/IR/InlineAsm.h"
-#include "llvm/ADT/StringExtras.h"
-#include "llvm/ADT/SmallString.h"
-#include "llvm/ADT/STLExtras.h"
+#include "CBackend.h"
 #include "llvm/Analysis/ConstantsScanner.h"
 #include "llvm/Analysis/FindUsedTypes.h"
-#include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/ValueTracking.h"
-#include "llvm/CodeGen/Passes.h"
-#include "llvm/CodeGen/IntrinsicLowering.h"
-#include "llvm/Transforms/Scalar.h"
-#include "llvm/MC/MCAsmInfo.h"
-#include "llvm/MC/MCContext.h"
-#include "llvm/MC/MCInstrInfo.h"
-#include "llvm/MC/MCObjectFileInfo.h"
-#include "llvm/MC/MCRegisterInfo.h"
-#include "llvm/MC/MCSubtargetInfo.h"
-#include "llvm/MC/MCSymbol.h"
-#include "llvm/IR/CallSite.h"
-#include "llvm/IR/CFG.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/ErrorHandling.h"
-#include "llvm/Support/FormattedStream.h"
-#include "llvm/IR/GetElementPtrTypeIterator.h"
 #include "llvm/Support/MathExtras.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/Host.h"
@@ -81,291 +49,87 @@ extern "C" void LLVMInitializeCBackendTarget() {
   RegisterTargetMachine<CTargetMachine> X(TheCBackendTarget);
 }
 
-namespace {
-  class CBEMCAsmInfo : public MCAsmInfo {
-  public:
-    CBEMCAsmInfo() {
-      PrivateGlobalPrefix = "";
-    }
-  };
-
-  /// CWriter - This class is the main chunk of code that converts an LLVM
-  /// module to a C translation unit.
-  class CWriter : public FunctionPass, public InstVisitor<CWriter> {
-    std::string _Out;
-    raw_string_ostream Out;
-    raw_ostream &FileOut;
-    IntrinsicLowering *IL;
-    LoopInfo *LI;
-    const Module *TheModule;
-    const MCAsmInfo* TAsm;
-    const MCRegisterInfo *MRI;
-    const MCObjectFileInfo *MOFI;
-    MCContext *TCtx;
-    const DataLayout* TD;
-
-    std::map<const ConstantFP *, unsigned> FPConstantMap;
-    std::set<const Argument*> ByValParams;
-    unsigned FPCounter;
-    unsigned OpaqueCounter;
-
-    DenseMap<const Value*, unsigned> AnonValueNumbers;
-    unsigned NextAnonValueNumber;
-
-    /// UnnamedStructIDs - This contains a unique ID for each struct that is
-    /// either anonymous or has no name.
-    DenseMap<StructType*, unsigned> UnnamedStructIDs;
-    unsigned NextAnonStructNumber;
-
-    std::set<StructType*> StructTypes;
-    std::set<ArrayType*> ArrayTypes;
-
-    DenseMap<std::pair<FunctionType*, AttributeSet>, unsigned> UnnamedFunctionIDs;
-    unsigned NextFunctionNumber;
-
-    // This is used to keep track of intrinsics that get generated to a lowered
-    // function. We must generate the prototypes before the function body which
-    // will only be expanded on first use
-    std::vector<Function*> prototypesToGen;
-
-  public:
-    static char ID;
-    explicit CWriter(formatted_raw_ostream &o)
-      : FunctionPass(ID), Out(_Out), FileOut(o), IL(0), LI(0),
-        TheModule(0), TAsm(0), MRI(0), MOFI(0), TCtx(0), TD(0),
-        OpaqueCounter(0), NextAnonValueNumber(0),
-        NextAnonStructNumber(0), NextFunctionNumber(0) {
-      initializeLoopInfoPass(*PassRegistry::getPassRegistry());
-      FPCounter = 0;
-    }
-
-    virtual const char *getPassName() const { return "C backend"; }
-
-    void getAnalysisUsage(AnalysisUsage &AU) const {
-      AU.addRequired<LoopInfo>();
-      AU.setPreservesAll();
-    }
-
-    virtual bool doInitialization(Module &M);
-
-    bool runOnFunction(Function &F) {
-     // Do not codegen any 'available_externally' functions at all, they have
-     // definitions outside the translation unit.
-     if (F.hasAvailableExternallyLinkage())
-       return false;
-
-      LI = &getAnalysis<LoopInfo>();
-
-      // Get rid of intrinsics we can't handle.
-      lowerIntrinsics(F);
-
-      // Output all floating point constants that cannot be printed accurately.
-      printFloatingPointConstants(F);
-
-      printFunction(F);
-      return false;
-    }
-
-    virtual bool doFinalization(Module &M);
-    void generateHeader();
-
-    raw_ostream &printFunctionProto(raw_ostream &Out, FunctionType *Ty,
-                           AttributeSet PAL, const std::string &Name);
-    raw_ostream &printFunctionProto(raw_ostream &Out, const Function *F) {
-      return printFunctionProto(Out, F->getFunctionType(), F->getAttributes(), GetValueName(F));
-    }
-
-    raw_ostream &printFunctionDeclaration(raw_ostream &Out, FunctionType *Ty,
-                           AttributeSet PAL, const std::string &Name);
-    raw_ostream &printStructDeclaration(raw_ostream &Out, StructType *Ty);
-    raw_ostream &printArrayDeclaration(raw_ostream &Out, ArrayType *Ty);
-
-    raw_ostream &printTypeName(raw_ostream &Out, Type *Ty, bool isSigned = false, AttributeSet PAL = AttributeSet());
-    raw_ostream &printSimpleType(raw_ostream &Out, Type *Ty, bool isSigned);
-
-    std::string getStructName(StructType *ST);
-    std::string getFunctionName(FunctionType *FT, AttributeSet PAL);
-    std::string getArrayName(ArrayType *AT);
-
-
-    /// writeOperandDeref - Print the result of dereferencing the specified
-    /// operand with '*'.  This is equivalent to printing '*' then using
-    /// writeOperand, but avoids excess syntax in some cases.
-    void writeOperandDeref(const Value *Operand) {
-      if (isAddressExposed(Operand)) {
-        // Already something with an address exposed.
-        writeOperandInternal(Operand);
-      } else {
-        Out << "*(";
-        writeOperand(Operand);
-        Out << ")";
-      }
-    }
-
-    void writeOperand(const Value *Operand, bool Static = false);
-    void writeInstComputationInline(const Instruction &I);
-    void writeOperandInternal(const Value *Operand, bool Static = false);
-    void writeOperandWithCast(const Value* Operand, unsigned Opcode);
-    void writeOperandWithCast(const Value* Operand, const ICmpInst &I);
-    bool writeInstructionCast(const Instruction &I);
-
-    void writeMemoryAccess(const Value *Operand, Type *OperandType,
-                           bool IsVolatile, unsigned Alignment);
-
-  private :
-    std::string InterpretASMConstraint(InlineAsm::ConstraintInfo& c);
-
-    void lowerIntrinsics(Function &F);
-    /// Prints the definition of the intrinsic function F. Supports the
-    /// intrinsics which need to be explicitly defined in the CBackend.
-    void printIntrinsicDefinition(const Function &F, raw_ostream &Out);
-
-    void printModuleTypes(raw_ostream &Out);
-    void printContainedStructs(raw_ostream &Out, Type *Ty, SmallPtrSet<Type *, 16> &);
-    void printFunctionSignature(raw_ostream &Out, const Function *F);
-
-    void printFloatingPointConstants(Function &F);
-    void printFloatingPointConstants(const Constant *C);
-
-    void printFunction(Function &);
-    void printBasicBlock(BasicBlock *BB);
-    void printLoop(Loop *L);
-
-    void printCast(unsigned opcode, Type *SrcTy, Type *DstTy);
-    void printConstant(const Constant *CPV, bool Static);
-    void printConstantWithCast(const Constant *CPV, unsigned Opcode);
-    bool printConstExprCast(const ConstantExpr *CE, bool Static);
-    void printConstantArray(const ConstantArray *CPA, bool Static);
-    void printConstantVector(const ConstantVector *CV, bool Static);
-    void printConstantDataSequential(const ConstantDataSequential *CDS, bool Static);
-
-
-    /// isAddressExposed - Return true if the specified value's name needs to
-    /// have its address taken in order to get a C value of the correct type.
-    /// This happens for global variables, byval parameters, and direct allocas.
-    bool isAddressExposed(const Value *V) const {
-      if (const Argument *A = dyn_cast<Argument>(V))
-        return ByValParams.count(A);
-      return isa<GlobalVariable>(V) || isDirectAlloca(V);
-    }
-
-    // isInlinableInst - Attempt to inline instructions into their uses to build
-    // trees as much as possible.  To do this, we have to consistently decide
-    // what is acceptable to inline, so that variable declarations don't get
-    // printed and an extra copy of the expr is not emitted.
-    //
-    static bool isInlinableInst(const Instruction &I) {
-      // Always inline cmp instructions, even if they are shared by multiple
-      // expressions.  GCC generates horrible code if we don't.
-      if (isa<CmpInst>(I))
-        return true;
-
-      // Must be an expression, must be used exactly once.  If it is dead, we
-      // emit it inline where it would go.
-      if (I.getType() == Type::getVoidTy(I.getContext()) || !I.hasOneUse() ||
-          isa<TerminatorInst>(I) || isa<CallInst>(I) || isa<PHINode>(I) ||
-          isa<LoadInst>(I) || isa<VAArgInst>(I) || isa<InsertElementInst>(I) ||
-          isa<InsertValueInst>(I))
-        // Don't inline a load across a store or other bad things!
-        return false;
-
-      // Must not be used in inline asm, extractelement, or shufflevector.
-      if (I.hasOneUse()) {
-        const Instruction &User = cast<Instruction>(*I.user_back());
-        if (isInlineAsm(User) || isa<ExtractElementInst>(User) ||
-            isa<ShuffleVectorInst>(User))
-          return false;
-      }
-
-      // Only inline instruction it if it's use is in the same BB as the inst.
-      return I.getParent() == cast<Instruction>(I.user_back())->getParent();
-    }
-
-    // isDirectAlloca - Define fixed sized allocas in the entry block as direct
-    // variables which are accessed with the & operator.  This causes GCC to
-    // generate significantly better code than to emit alloca calls directly.
-    //
-    static const AllocaInst *isDirectAlloca(const Value *V) {
-      const AllocaInst *AI = dyn_cast<AllocaInst>(V);
-      if (!AI) return 0;
-      if (AI->isArrayAllocation())
-        return 0;   // FIXME: we can also inline fixed size array allocas!
-      if (AI->getParent() != &AI->getParent()->getParent()->getEntryBlock())
-        return 0;
-      return AI;
-    }
-
-    // isInlineAsm - Check if the instruction is a call to an inline asm chunk.
-    static bool isInlineAsm(const Instruction& I) {
-      if (const CallInst *CI = dyn_cast<CallInst>(&I))
-        return isa<InlineAsm>(CI->getCalledValue());
-      return false;
-    }
-
-    // Instruction visitation functions
-    friend class InstVisitor<CWriter>;
-
-    void visitReturnInst(ReturnInst &I);
-    void visitBranchInst(BranchInst &I);
-    void visitSwitchInst(SwitchInst &I);
-    void visitIndirectBrInst(IndirectBrInst &I);
-    void visitInvokeInst(InvokeInst &I) {
-      llvm_unreachable("Lowerinvoke pass didn't work!");
-    }
-    void visitResumeInst(ResumeInst &I) {
-      llvm_unreachable("DwarfEHPrepare pass didn't work!");
-    }
-    void visitUnreachableInst(UnreachableInst &I);
-
-    void visitPHINode(PHINode &I);
-    void visitBinaryOperator(Instruction &I);
-    void visitICmpInst(ICmpInst &I);
-    void visitFCmpInst(FCmpInst &I);
-
-    void visitCastInst (CastInst &I);
-    void visitSelectInst(SelectInst &I);
-    void visitCallInst (CallInst &I);
-    void visitInlineAsm(CallInst &I);
-    bool visitBuiltinCall(CallInst &I, Intrinsic::ID ID);
-
-    void visitAllocaInst(AllocaInst &I);
-    void visitLoadInst  (LoadInst   &I);
-    void visitStoreInst (StoreInst  &I);
-    void visitGetElementPtrInst(GetElementPtrInst &I);
-    void visitVAArgInst (VAArgInst &I);
-
-    void visitInsertElementInst(InsertElementInst &I);
-    void visitExtractElementInst(ExtractElementInst &I);
-    void visitShuffleVectorInst(ShuffleVectorInst &SVI);
-
-    void visitInsertValueInst(InsertValueInst &I);
-    void visitExtractValueInst(ExtractValueInst &I);
-
-    void visitInstruction(Instruction &I) {
-#ifndef NDEBUG
-      errs() << "C Writer does not know about " << I;
-#endif
-      llvm_unreachable(0);
-    }
-
-    void outputLValue(Instruction *I) {
-      Out << "  " << GetValueName(I) << " = ";
-    }
-
-    bool isGotoCodeNecessary(BasicBlock *From, BasicBlock *To);
-    void printPHICopiesForSuccessor(BasicBlock *CurBlock,
-                                    BasicBlock *Successor, unsigned Indent);
-    void printBranchToBlock(BasicBlock *CurBlock, BasicBlock *SuccBlock,
-                            unsigned Indent);
-    void printGEPExpression(Value *Ptr, gep_type_iterator I,
-                            gep_type_iterator E, bool Static);
-
-    std::string GetValueName(const Value *Operand);
-  };
-}
-
 char CWriter::ID = 0;
 
+/// isAddressExposed - Return true if the specified value's name needs to
+/// have its address taken in order to get a C value of the correct type.
+/// This happens for global variables, byval parameters, and direct allocas.
+bool CWriter::isAddressExposed(const Value *V) const {
+  if (const Argument *A = dyn_cast<Argument>(V))
+    return ByValParams.count(A);
+  return isa<GlobalVariable>(V) || isDirectAlloca(V);
+}
+
+// isInlinableInst - Attempt to inline instructions into their uses to build
+// trees as much as possible.  To do this, we have to consistently decide
+// what is acceptable to inline, so that variable declarations don't get
+// printed and an extra copy of the expr is not emitted.
+//
+bool CWriter::isInlinableInst(const Instruction &I) {
+  // Always inline cmp instructions, even if they are shared by multiple
+  // expressions.  GCC generates horrible code if we don't.
+  if (isa<CmpInst>(I))
+    return true;
+
+  // Must be an expression, must be used exactly once.  If it is dead, we
+  // emit it inline where it would go.
+  if (I.getType() == Type::getVoidTy(I.getContext()) || !I.hasOneUse() ||
+      isa<TerminatorInst>(I) || isa<CallInst>(I) || isa<PHINode>(I) ||
+      isa<LoadInst>(I) || isa<VAArgInst>(I) || isa<InsertElementInst>(I) ||
+      isa<InsertValueInst>(I))
+    // Don't inline a load across a store or other bad things!
+    return false;
+
+  // Must not be used in inline asm, extractelement, or shufflevector.
+  if (I.hasOneUse()) {
+    const Instruction &User = cast<Instruction>(*I.user_back());
+    if (isInlineAsm(User) || isa<ExtractElementInst>(User) ||
+        isa<ShuffleVectorInst>(User))
+      return false;
+  }
+
+  // Only inline instruction it if it's use is in the same BB as the inst.
+  return I.getParent() == cast<Instruction>(I.user_back())->getParent();
+}
+
+// isDirectAlloca - Define fixed sized allocas in the entry block as direct
+// variables which are accessed with the & operator.  This causes GCC to
+// generate significantly better code than to emit alloca calls directly.
+//
+const AllocaInst *CWriter::isDirectAlloca(const Value *V) const {
+  const AllocaInst *AI = dyn_cast<AllocaInst>(V);
+  if (!AI) return 0;
+  if (AI->isArrayAllocation())
+    return 0;   // FIXME: we can also inline fixed size array allocas!
+  if (AI->getParent() != &AI->getParent()->getParent()->getEntryBlock())
+    return 0;
+  return AI;
+}
+
+// isInlineAsm - Check if the instruction is a call to an inline asm chunk.
+bool CWriter::isInlineAsm(const Instruction& I) {
+  if (const CallInst *CI = dyn_cast<CallInst>(&I))
+    return isa<InlineAsm>(CI->getCalledValue());
+  return false;
+}
+
+bool CWriter::runOnFunction(Function &F) {
+ // Do not codegen any 'available_externally' functions at all, they have
+ // definitions outside the translation unit.
+ if (F.hasAvailableExternallyLinkage())
+   return false;
+
+  LI = &getAnalysis<LoopInfo>();
+
+  // Get rid of intrinsics we can't handle.
+  lowerIntrinsics(F);
+
+  // Output all floating point constants that cannot be printed accurately.
+  printFloatingPointConstants(F);
+
+  printFunction(F);
+  return false;
+}
 
 static std::string CBEMangle(const std::string &S) {
   std::string Result;
@@ -1362,6 +1126,20 @@ void CWriter::writeOperand(const Value *Operand, bool Static) {
 
   if (isAddressImplicit)
     Out << ')';
+}
+
+/// writeOperandDeref - Print the result of dereferencing the specified
+/// operand with '*'.  This is equivalent to printing '*' then using
+/// writeOperand, but avoids excess syntax in some cases.
+void CWriter::writeOperandDeref(const Value *Operand) {
+  if (isAddressExposed(Operand)) {
+    // Already something with an address exposed.
+    writeOperandInternal(Operand);
+  } else {
+    Out << "*(";
+    writeOperand(Operand);
+    Out << ")";
+  }
 }
 
 // Some instructions need to have their result value casted back to the
@@ -2447,7 +2225,7 @@ void CWriter::visitIndirectBrInst(IndirectBrInst &IBI) {
 }
 
 void CWriter::visitUnreachableInst(UnreachableInst &I) {
-  Out << "__builtin_unreachable();\n";
+  Out << "__builtin_unreachable();\n"; // TODO: use __assume(0) on MSVC
 }
 
 bool CWriter::isGotoCodeNecessary(BasicBlock *From, BasicBlock *To) {
---
+++ lib/Target/CBackend/CBackend.h
@@ -0,0 +1,230 @@
+
+#include "CTargetMachine.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/CodeGen/IntrinsicLowering.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/CallingConv.h"
+#include "llvm/IR/CallSite.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/GetElementPtrTypeIterator.h"
+#include "llvm/IR/InstVisitor.h"
+#include "llvm/IR/InlineAsm.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Intrinsics.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/Module.h"
+#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCObjectFileInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/Pass.h"
+#include "llvm/PassManager.h"
+#include "llvm/Support/FormattedStream.h"
+#include "llvm/Transforms/Scalar.h"
+
+namespace {
+  using namespace llvm;
+
+  class CBEMCAsmInfo : public MCAsmInfo {
+  public:
+    CBEMCAsmInfo() {
+      PrivateGlobalPrefix = "";
+    }
+  };
+
+  /// CWriter - This class is the main chunk of code that converts an LLVM
+  /// module to a C translation unit.
+  class CWriter : public FunctionPass, public InstVisitor<CWriter> {
+    std::string _Out;
+    raw_string_ostream Out;
+    raw_ostream &FileOut;
+    IntrinsicLowering *IL;
+    LoopInfo *LI;
+    const Module *TheModule;
+    const MCAsmInfo* TAsm;
+    const MCRegisterInfo *MRI;
+    const MCObjectFileInfo *MOFI;
+    MCContext *TCtx;
+    const DataLayout* TD;
+
+    std::map<const ConstantFP *, unsigned> FPConstantMap;
+    std::set<const Argument*> ByValParams;
+    unsigned FPCounter;
+    unsigned OpaqueCounter;
+
+    DenseMap<const Value*, unsigned> AnonValueNumbers;
+    unsigned NextAnonValueNumber;
+
+    /// UnnamedStructIDs - This contains a unique ID for each struct that is
+    /// either anonymous or has no name.
+    DenseMap<StructType*, unsigned> UnnamedStructIDs;
+    unsigned NextAnonStructNumber;
+
+    std::set<StructType*> StructTypes;
+    std::set<ArrayType*> ArrayTypes;
+
+    DenseMap<std::pair<FunctionType*, AttributeSet>, unsigned> UnnamedFunctionIDs;
+    unsigned NextFunctionNumber;
+
+    // This is used to keep track of intrinsics that get generated to a lowered
+    // function. We must generate the prototypes before the function body which
+    // will only be expanded on first use
+    std::vector<Function*> prototypesToGen;
+
+  public:
+    static char ID;
+    explicit CWriter(formatted_raw_ostream &o)
+      : FunctionPass(ID), Out(_Out), FileOut(o), IL(0), LI(0),
+        TheModule(0), TAsm(0), MRI(0), MOFI(0), TCtx(0), TD(0),
+        OpaqueCounter(0), NextAnonValueNumber(0),
+        NextAnonStructNumber(0), NextFunctionNumber(0) {
+      initializeLoopInfoPass(*PassRegistry::getPassRegistry());
+      FPCounter = 0;
+    }
+
+    virtual const char *getPassName() const { return "C backend"; }
+
+    void getAnalysisUsage(AnalysisUsage &AU) const {
+      AU.addRequired<LoopInfo>();
+      AU.setPreservesAll();
+    }
+
+    virtual bool doInitialization(Module &M);
+    virtual bool doFinalization(Module &M);
+
+    bool runOnFunction(Function &F);
+
+    void generateHeader();
+
+    raw_ostream &printFunctionProto(raw_ostream &Out, FunctionType *Ty,
+                           AttributeSet PAL, const std::string &Name);
+    raw_ostream &printFunctionProto(raw_ostream &Out, const Function *F) {
+      return printFunctionProto(Out, F->getFunctionType(), F->getAttributes(), GetValueName(F));
+    }
+
+    raw_ostream &printFunctionDeclaration(raw_ostream &Out, FunctionType *Ty,
+                           AttributeSet PAL, const std::string &Name);
+    raw_ostream &printStructDeclaration(raw_ostream &Out, StructType *Ty);
+    raw_ostream &printArrayDeclaration(raw_ostream &Out, ArrayType *Ty);
+
+    raw_ostream &printTypeName(raw_ostream &Out, Type *Ty, bool isSigned = false, AttributeSet PAL = AttributeSet());
+    raw_ostream &printSimpleType(raw_ostream &Out, Type *Ty, bool isSigned);
+
+    std::string getStructName(StructType *ST);
+    std::string getFunctionName(FunctionType *FT, AttributeSet PAL);
+    std::string getArrayName(ArrayType *AT);
+
+
+    void writeOperandDeref(const Value *Operand);
+    void writeOperand(const Value *Operand, bool Static = false);
+    void writeInstComputationInline(const Instruction &I);
+    void writeOperandInternal(const Value *Operand, bool Static = false);
+    void writeOperandWithCast(const Value* Operand, unsigned Opcode);
+    void writeOperandWithCast(const Value* Operand, const ICmpInst &I);
+    bool writeInstructionCast(const Instruction &I);
+    void writeMemoryAccess(const Value *Operand, Type *OperandType,
+                           bool IsVolatile, unsigned Alignment);
+
+  private :
+    std::string InterpretASMConstraint(InlineAsm::ConstraintInfo& c);
+
+    void lowerIntrinsics(Function &F);
+    /// Prints the definition of the intrinsic function F. Supports the
+    /// intrinsics which need to be explicitly defined in the CBackend.
+    void printIntrinsicDefinition(const Function &F, raw_ostream &Out);
+
+    void printModuleTypes(raw_ostream &Out);
+    void printContainedStructs(raw_ostream &Out, Type *Ty, SmallPtrSet<Type *, 16> &);
+    void printFunctionSignature(raw_ostream &Out, const Function *F);
+
+    void printFloatingPointConstants(Function &F);
+    void printFloatingPointConstants(const Constant *C);
+
+    void printFunction(Function &);
+    void printBasicBlock(BasicBlock *BB);
+    void printLoop(Loop *L);
+
+    void printCast(unsigned opcode, Type *SrcTy, Type *DstTy);
+    void printConstant(const Constant *CPV, bool Static);
+    void printConstantWithCast(const Constant *CPV, unsigned Opcode);
+    bool printConstExprCast(const ConstantExpr *CE, bool Static);
+    void printConstantArray(const ConstantArray *CPA, bool Static);
+    void printConstantVector(const ConstantVector *CV, bool Static);
+    void printConstantDataSequential(const ConstantDataSequential *CDS, bool Static);
+
+    bool isAddressExposed(const Value *V) const;
+    bool isInlinableInst(const Instruction &I);
+    const AllocaInst *isDirectAlloca(const Value *V) const;
+    bool isInlineAsm(const Instruction& I);
+
+    // Instruction visitation functions
+    friend class InstVisitor<CWriter>;
+
+    void visitReturnInst(ReturnInst &I);
+    void visitBranchInst(BranchInst &I);
+    void visitSwitchInst(SwitchInst &I);
+    void visitIndirectBrInst(IndirectBrInst &I);
+    void visitInvokeInst(InvokeInst &I) {
+      llvm_unreachable("Lowerinvoke pass didn't work!");
+    }
+    void visitResumeInst(ResumeInst &I) {
+      llvm_unreachable("DwarfEHPrepare pass didn't work!");
+    }
+    void visitUnreachableInst(UnreachableInst &I);
+
+    void visitPHINode(PHINode &I);
+    void visitBinaryOperator(Instruction &I);
+    void visitICmpInst(ICmpInst &I);
+    void visitFCmpInst(FCmpInst &I);
+
+    void visitCastInst (CastInst &I);
+    void visitSelectInst(SelectInst &I);
+    void visitCallInst (CallInst &I);
+    void visitInlineAsm(CallInst &I);
+    bool visitBuiltinCall(CallInst &I, Intrinsic::ID ID);
+
+    void visitAllocaInst(AllocaInst &I);
+    void visitLoadInst  (LoadInst   &I);
+    void visitStoreInst (StoreInst  &I);
+    void visitGetElementPtrInst(GetElementPtrInst &I);
+    void visitVAArgInst (VAArgInst &I);
+
+    void visitInsertElementInst(InsertElementInst &I);
+    void visitExtractElementInst(ExtractElementInst &I);
+    void visitShuffleVectorInst(ShuffleVectorInst &SVI);
+
+    void visitInsertValueInst(InsertValueInst &I);
+    void visitExtractValueInst(ExtractValueInst &I);
+
+    void visitInstruction(Instruction &I) {
+#ifndef NDEBUG
+      errs() << "C Writer does not know about " << I;
+#endif
+      llvm_unreachable(0);
+    }
+
+    void outputLValue(Instruction *I) {
+      Out << "  " << GetValueName(I) << " = ";
+    }
+
+    bool isGotoCodeNecessary(BasicBlock *From, BasicBlock *To);
+    void printPHICopiesForSuccessor(BasicBlock *CurBlock,
+                                    BasicBlock *Successor, unsigned Indent);
+    void printBranchToBlock(BasicBlock *CurBlock, BasicBlock *SuccBlock,
+                            unsigned Indent);
+    void printGEPExpression(Value *Ptr, gep_type_iterator I,
+                            gep_type_iterator E, bool Static);
+
+    std::string GetValueName(const Value *Operand);
+  };
+}
