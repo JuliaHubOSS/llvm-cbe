8ae607734c0a2a86fede91080036b1e5a0695810
Y. Sapir <yasapir@gmail.com>
2018-10-30 12:55:09 +0200

    Remove reference to missing APInt-C.h

--- lib/Target/CBackend/CBackend.cpp
+++ lib/Target/CBackend/CBackend.cpp
@@ -1811,7 +1811,6 @@ void CWriter::generateHeader(Module &M) {
   Out << "#include <limits.h>\n";      // With overflow intrinsics support.
   Out << "#include <stdint.h>\n";      // Sized integer support
   Out << "#include <math.h>\n";        // definitions for some math functions and numeric constants
-  Out << "#include <APInt-C.h>\n";     // Implementations of many llvm intrinsics
   // Provide a definition for `bool' if not compiling with a C++ compiler.
   Out << "#ifndef __cplusplus\ntypedef unsigned char bool;\n#endif\n";
   Out << "\n";
