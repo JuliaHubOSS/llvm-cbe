0c55add5846e2cbe81c609253559fcb89d742ade
Y. Sapir <yasapir@gmail.com>
2018-10-30 14:20:30 +0200

    Add test for mutually referring globals (#4)

---
+++ test/c_tests/other/test_mutual_ref_globals.c
@@ -0,0 +1,28 @@
+//===---------------------- LLVM C Backend test file ----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This code tests to see that the CBE will properly initialize global variables
+// that reference each other. (Issue #4)
+//
+//===----------------------------------------------------------------------===//
+
+extern void* pA;
+extern void* pB;
+void* pA = &pB;
+void* pB = &pA;
+
+int main() {
+  if (**(void***)pA != pA) {
+    return 1;
+  }
+  if (**(void***)pB != pB) {
+    return 2;
+  }
+  return 6;
+}
