6ea0855339fcd7ad4dc78f653e448fbe386943eb
noah the goodra <peterpan0413@live.com>
2021-07-05 14:16:21 -0500

    handle file access constants

--- tools/llvm-cbe/llvm-cbe.cpp
+++ tools/llvm-cbe/llvm-cbe.cpp
@@ -185,9 +185,17 @@ static ToolOutputFile *GetOutputStream(const char *TargetName,
 
   // Open the file.
   std::error_code error;
+#if LLVM_VERSION_MAJOR > 10
   sys::fs::OpenFlags OpenFlags = sys::fs::OF_None;
+#else
+  sys::fs::OpenFlags OpenFlags = sys::fs::F_None;
+#endif
   if (Binary)
+#if LLVM_VERSION_MAJOR > 10
     OpenFlags |= sys::fs::OF_Text;
+#else
+  OpenFlags |= sys::fs::F_Text;
+#endif
   ToolOutputFile *FDOut =
       new ToolOutputFile(OutputFilename.c_str(), error, OpenFlags);
   if (error) {
