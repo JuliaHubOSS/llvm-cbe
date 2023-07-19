563c76556020418c03a636fa9edc84201622457e
Daniel Paoliello <danpao@microsoft.com>
2023-07-19 14:25:26 -0700

    Move to LLVM's license, delete LLVMBuild files

--- lib/Target/CBackend/IDMap.h
+++ lib/Target/CBackend/IDMap.h
@@ -1,3 +1,13 @@
+//===------------------ IDMap.h----------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the Apache License v2.0 with LLVM Exceptions.
+// See LICENSE.TXT for details.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "llvm/ADT/DenseMap.h"
 
 namespace llvm_cbe {
